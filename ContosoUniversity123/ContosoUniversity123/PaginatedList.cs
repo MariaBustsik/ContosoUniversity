c3eb020dd73d98026c839ce06cf20704221ddaeb
Maria Bustsik <maria.bustsik@outlook.com>
2021-10-18 13:35:07 +0300

    .

--- ContosoUniversity123/ContosoUniversity123/PaginatedList.cs
+++ ContosoUniversity123/ContosoUniversity123/PaginatedList.cs
@@ -3,9 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
-using ContosoUniversity.Data;
-using ContosoUniversity.Models;
-using ContosoUniversity;
+
 
 namespace ContosoUniversity
 {
